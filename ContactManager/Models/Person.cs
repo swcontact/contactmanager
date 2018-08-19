5c5b1ba533fda2db73accad1a93573e4843b6967
Huey Zhou <huey.zhou@ibm.com>
2018-08-19 00:42:11 -0400

    Changed controller to contact and using standard http methods

--- ContactManager/Models/Person.cs
+++ ContactManager/Models/Person.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContactManager.Models
 {
     public class Person
     {
         public long ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Birthday { get; set; }
+
         public string Email { get; set; }
+
         public string Telephone { get; set; }
     }
 }
